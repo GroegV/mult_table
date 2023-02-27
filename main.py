366a197c13f80de3b59d56e9b69aed22ffc857ca
GroegV <adygezalmamedov@gmail.com>
2023-02-27 15:52:58 +0500

    initial commit

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 #n = 2
-print(f"Таблица умножения на {2}")
+import os
+from random import randint
+print(f"Таблица умножения на 2:")
 for i in range(1, 10):
     print(f"2 x {i} = {2*i}")
+
+input("Нажмите Enter для продолжения...")
+    
+os.system("cls")
+
+print("Выполните задания:")
+for i in range(1, 10):
+    answer = input(f"2 x {i} = ")
+    if int(answer) == 2*i:
+        print("Правильно!")
+    else:
+        print("Неверно!")
