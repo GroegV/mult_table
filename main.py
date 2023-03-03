279b0635accf4a717b274d2a6532326fb7a3e8d2
GroegV <adygezalmamedov@gmail.com>
2023-03-03 16:11:14 +0500

    anothernums commit

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-#n = 2
 import os
 from random import randint
-print(f"Таблица умножения на 2:")
-for i in range(1, 10):
-    print(f"2 x {i} = {2*i}")
+for n in range(2, 11):
+    print(f"Таблица умножения на {n}:")
+    for i in range(1, 10):
+        print(f"{n} x {i} = {n*i}")
 
-input("Нажмите Enter для продолжения...")
+    input("Нажмите Enter для продолжения...")
     
-os.system("cls")
+    os.system("cls")
 
-print("Выполните задания:")
-for i in range(1, 10):
-    answer = input(f"2 x {i} = ")
-    if int(answer) == 2*i:
-        print("Правильно!")
-    else:
-        print("Неверно!")
+    print("Выполните задания:")
+    for i in range(1, 10):
+       answer = input(f"{n} x {i} = ")
+       if int(answer) == n*i:
+            print("Правильно!")
+       else:
+           print("Неверно!")
